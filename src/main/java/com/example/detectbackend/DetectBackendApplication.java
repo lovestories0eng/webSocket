2c6018a54c41dff69066ebe1965b5a9f5fbb3ac1
lovestroies0eng <1494121350@qq.com>
2023-03-24 22:51:13 +0800

    project built

---
+++ src/main/java/com/example/detectbackend/DetectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.detectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetectBackendApplication.class, args);
+    }
+
+}
